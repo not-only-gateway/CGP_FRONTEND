3c49336d6af808db2b49704d8093914d3ba83967
facobackup <gustavomicaelbarbosa@gmail.com>
2022-05-30 09:40:12 -0300

    Ajustes

--- components/CollaboratorList.js
+++ components/CollaboratorList.js
@@ -1,6 +1,6 @@
-import React, {useContext, useEffect, useMemo, useRef, useState} from "react";
+import React, {useCallback, useContext, useEffect, useMemo, useRef, useState} from "react";
 import PropTypes from "prop-types";
-import {Button, DataRow, Modal, Switcher} from "@f-ui/core";
+import {Button, DataRow, Modal, Switcher, Tab, Tabs} from "@f-ui/core";
 
 import getQuery from "../utils/getQuery";
 import {KEYS} from "../templates/KEYS";
@@ -13,6 +13,8 @@ import page from "../public/page.json";
 import Cookies from "universal-cookie/lib";
 import AdminContext from "../ext/wrapper/AdminContext";
 import QRCode from 'qrcode'
+import html2canvas from "html2canvas";
+
 
 export default function CollaboratorList() {
     const isADM = useContext(AdminContext)
@@ -117,46 +119,102 @@ CollaboratorList.propTypes = {
 
 
 function User({current, keys}) {
-    const ref = useRef()
-    useEffect(() => {
-        QRCode.toCanvas(ref.current, getVCARD(current.name, current.email, current.extension), (err) => console.log(err))
+    const ref = useCallback(node => {
+        if (node)
+            QRCode.toCanvas(node, getVCARD(current.name, current.email, current.extension), (err) => console.log(err))
     }, [])
+    const canvasRef = useRef()
+    const [open, setOpen] = useState(0)
+
 
     return (
-        <>
-            <Avatar
-                alt={current?.name}
-                src={current?.image}
-                size={"huge"}
-                outlined={true}
-            />
-            <h2>{current?.name}</h2>
-            {current ?
-                <DataRow
-                    keys={keys.filter(k => k.key !== 'image')}
-                    asCard={true} selfContained={true}
-                    object={current}
-                    className={styles.selectedCard}
-                /> : undefined}
+        <Tabs open={open} setOpen={setOpen}>
+            <Tab label={'Dados'} className={styles.modalTab}>
+                <Avatar
+                    alt={current?.name}
+                    src={current?.image}
+                    size={"huge"}
+                    outlined={true}
+                />
+                <h2>{current?.name}</h2>
+                {current ?
+                    <DataRow
+                        keys={keys.filter(k => k.key !== 'image')}
+                        asCard={true} selfContained={true}
+                        object={current}
+                        className={styles.selectedCard}
+                    />
+                    :
+                    undefined
+                }
+            </Tab>
+            <Tab label={'Cartão virtual'} className={styles.modalTab}>
+                <fieldset className={styles.qrCode}>
+                    <legend>Cartão virtual</legend>
+                    <div ref={canvasRef} className={styles.cardWrapper}>
 
-          <div className={styles.qrCode}>
-              <canvas ref={ref}/>
-              <Button variant={"filled"}
-                      className={styles.buttonDownload}
-                      onClick={(e) => {
-                          const element = document.createElement('a');
-                          element.setAttribute('href', ref.current.toDataURL());
-                          element.setAttribute('download', 'QR-CODE-' + current.name);
-                          element.style.display = 'none';
-                          document.body.appendChild(element);
-                          element.click();
-                          document.body.removeChild(element);
-                      }}>
-                  <span className={'material-icons-round'} style={{fontSize: '1.1rem'}}>file_download</span>
-                  Download
-              </Button>
-          </div>
-        </>
+                        <img src={'./ass.png'} alt={'assinatura'} className={styles.imgData}/>
+                        <div className={styles.cardInfo}>
+                            <div style={{fontSize: '8px'}}>
+                                {current.name}
+                            </div>
+                            <div>
+                                {current.role}
+                            </div>
+                            <div>
+                                {current.unit?.name}
+                            </div>
+                            <div>
+                                {current.directory}
+                            </div>
+                            <div style={{fontSize: '8px'}}>
+                                Agência Espacial Brasileira
+                            </div>
+                            <div>
+                                (61) 2033-{current.extension}&nbsp;&nbsp;&nbsp;&nbsp;{current.email}
+                            </div>
+                        </div>
+                    </div>
+                    {/*<canvas ref={canvasRef} className={styles.canvas} width={'890'} height={'300'}/>*/}
+                    <Button variant={"filled"}
+                            className={styles.buttonDownload}
+                            onClick={(e) => {
+                                console.log(canvasRef.current)
+                                html2canvas(canvasRef.current, { scale:4, backgroundColor: null }).then(canvas =>  {
+                                    document.body.appendChild(canvas);
+                                    const element = document.createElement('a');
+                                    element.setAttribute('href', canvas.toDataURL());
+                                    element.setAttribute('download', 'Cartão virtual' + current.name);
+                                    element.style.display = 'none';
+                                    document.body.appendChild(element);
+                                    element.click();
+                                    document.body.removeChild(element);
+                                })
+                            }}>
+                        <span className={'material-icons-round'} style={{fontSize: '1.1rem'}}>file_download</span>
+                        Download
+                    </Button>
+                </fieldset>
+                <fieldset className={styles.qrCode}>
+                    <legend>Contato</legend>
+                    <canvas ref={ref}/>
+                    <Button variant={"filled"}
+                            className={styles.buttonDownload}
+                            onClick={(e) => {
+                                const element = document.createElement('a');
+                                element.setAttribute('href', e.target.previousSibling.toDataURL());
+                                element.setAttribute('download', 'QR-CODE-' + current.name);
+                                element.style.display = 'none';
+                                document.body.appendChild(element);
+                                element.click();
+                                document.body.removeChild(element);
+                            }}>
+                        <span className={'material-icons-round'} style={{fontSize: '1.1rem'}}>file_download</span>
+                        Download
+                    </Button>
+                </fieldset>
+            </Tab>
+        </Tabs>
     )
 }
 
@@ -165,8 +223,9 @@ function getVCARD(name, email, ramal) {
     return `BEGIN:VCARD
 N: ${splitName[splitName.length - 1]}; ${splitName[0]};;;
 FN: ${name}
-TITLE: Agência Espacial Brasileira
+TITLE: Agencia Espacial Brasileira
 TEL;WORK,VOICE:61 2033-${ramal}
 EMAIL;INTERNET,HOME:${email}
+ADR;WORK:;;SPO, Setor Policial, Área 5 Quadra 3 BL A, SHCS, Brasília - DF, 70610-200;;;;
 END:VCARD`
 }
